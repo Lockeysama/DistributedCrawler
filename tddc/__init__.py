8a693c30c3fc9eac48c5577460db4560a4167791
陈奕涛 <chenyitao@chenyitaodeMacbook-Pro.local>
2018-08-14 15:33:44 +0800

    [add] 新增SnowFlakeID

--- tddc/__init__.py
+++ tddc/__init__.py
@@ -7,6 +7,7 @@ from .log import logger
 from .redis.redis_client import *
 from .util.short_uuid import *
 from .util.util import *
+from .util.snowflake import *
 from .worker import *
 
 __all__ = ['ExceptionCollection',
@@ -17,6 +18,7 @@ __all__ = ['ExceptionCollection',
            'RedisClient',
            'StatusManager',
            'ShortUUID',
+           'SnowFlakeID',
            'Singleton', 'object2json', 'timer',
            'EventCenter',
            'ExternBase',
