b24bd39baff98270cb49c73806110a9363fd7b36
陈奕涛 <chenyitao@chenyitaodeMacbook-Pro.local>
2018-09-20 10:52:07 +0800

    [add] 新增设备信息收集

--- tddc/util/device_info.py
+++ tddc/util/device_info.py
@@ -7,3 +7,94 @@
 @file    : device_info.py
 @time    : 2018/9/10 18:10
 """
+import gevent
+import psutil
+import socket
+import netifaces
+
+
+class Device(object):
+
+    @classmethod
+    def ip(cls):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(('8.8.8.8', 80))
+            ip = s.getsockname()[0]
+        finally:
+            s.close()
+        return ip
+
+    @classmethod
+    def mac(cls):
+        for i in netifaces.interfaces():
+            address = netifaces.ifaddresses(i)
+            try:
+                if_mac = address[netifaces.AF_LINK][0]['addr']
+                if_ip = address[netifaces.AF_INET][0]['addr']
+            except Exception as e:
+                continue
+            if if_ip == Device.ip():
+                return if_mac
+        return None
+
+    @classmethod
+    def process_snapshot(cls):
+        snapshot = []
+        for p in psutil.process_iter():
+            try:
+                snapshot.append({
+                    'pid': p.pid,
+                    'name': p.name(),
+                    'cpu_percent': p.cpu_percent(),
+                    'rss': float('%.2f' % (p.memory_info().rss / 1024 / 1024)),
+                    'memory_percent': float('%.2f' % (p.memory_percent() * 100))
+                })
+            except Exception as e:
+                pass
+        return snapshot
+
+    @classmethod
+    def cpu_snapshot(cls):
+        return {
+            'cpu_count': psutil.cpu_count(),
+            'cpu_percent': psutil.cpu_percent(0, True)
+        }
+
+    @classmethod
+    def memory_snapshot(cls):
+        mem = psutil.virtual_memory()
+        return {
+            'mem_total': float('%.2f' % (mem.total / 1073741824.0)),
+            'mem_used': float('%.2f' % (mem.used / 1073741824.0)),
+            'mem_available': float('%.2f' % (mem.available / 1073741824.0)),
+            'mem_percent': mem.percent
+        }
+
+    @classmethod
+    def disk_snapshot(cls):
+        disk = psutil.disk_usage('/')
+        return {
+            'disk_total': float('%.2f' % (disk.total / 1073741824.0)),
+            'disk_used': float('%.2f' % (disk.used / 1073741824.0)),
+            'disk_free': float('%.2f' % (disk.free / 1073741824.0)),
+            'disk_percent': disk.percent
+        }
+
+    @classmethod
+    def net_snapshot(cls):
+        net_io_counters = psutil.net_io_counters()
+        return {
+            'send': net_io_counters.bytes_sent,
+            'recv': net_io_counters.bytes_recv
+        }
+
+    @classmethod
+    def net_rate(cls, interval=1):
+        old = Device.net_snapshot()
+        gevent.sleep(interval)
+        new = Device.net_snapshot()
+        rate = {}
+        for k, v in new.items():
+            rate[k] = float('%.2f' % ((v - old[k]) / 1024.0 / 1024))
+        return rate
--- tddc/worker/monitor.py
+++ tddc/worker/monitor.py
@@ -7,3 +7,93 @@
 @file    : monitor.py
 @time    : 2018/9/18 10:54
 """
+import json
+import time
+
+import gevent
+
+from ..util.time_helper import TimeHelper
+from ..util.device_info import Device
+from ..util.util import Singleton
+
+from .status import StatusManager
+
+
+class Monitor(object):
+
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        gevent.spawn(self.snapshot)
+        gevent.sleep()
+        gevent.spawn(self.cpu_usage_rate)
+        gevent.sleep()
+        gevent.spawn(self.memory_usage_rate)
+        gevent.sleep()
+        gevent.spawn(self.net_usage_rate)
+        gevent.sleep()
+
+    @staticmethod
+    def snapshot():
+        name = 'tddc:worker:monitor:snapshot:{}'.format(Device.ip())
+        while True:
+            min_ts = TimeHelper(time.time()).get_minute_timestamp()
+            date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(min_ts))
+            process_snapshot = Device.process_snapshot()
+            disk_snapshot = Device.disk_snapshot()
+            StatusManager().hmset(
+                name, {
+                    'process': json.dumps(process_snapshot),
+                    'disk': json.dumps(disk_snapshot),
+                    'date': date
+                }
+            )
+            gevent.sleep(60)
+
+    @staticmethod
+    def cpu_usage_rate():
+        name = 'tddc:worker:monitor:cpu_usage_rate:{}'.format(Device.ip())
+        while True:
+            min_ts = TimeHelper(time.time()).get_minute_timestamp()
+            date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(min_ts))
+            snapshot = Device.cpu_snapshot()
+            snapshot['date'] = date
+            StatusManager().hmset(name, {min_ts: json.dumps(snapshot)})
+            keys = StatusManager().hkeys(name)
+            if len(keys) > 1450:
+                keys.sort()
+                expire = keys[:len(keys) - 1440]
+                StatusManager().hmdel(name, expire)
+            gevent.sleep(60)
+
+    @staticmethod
+    def memory_usage_rate():
+        name = 'tddc:worker:monitor:memory_usage_rate:{}'.format(Device.ip())
+        while True:
+            min_ts = TimeHelper(time.time()).get_minute_timestamp()
+            date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(min_ts))
+            snapshot = Device.memory_snapshot()
+            snapshot['date'] = date
+            StatusManager().hmset(name, {min_ts: json.dumps(snapshot)})
+            keys = StatusManager().hkeys(name)
+            if len(keys) > 1450:
+                keys.sort()
+                expire = keys[:len(keys) - 1440]
+                StatusManager().hmdel(name, expire)
+            gevent.sleep(60)
+
+    @staticmethod
+    def net_usage_rate():
+        name = 'tddc:worker:monitor:net_usage_rate:{}'.format(Device.ip())
+        while True:
+            net_rate = Device.net_rate(60)
+            min_ts = TimeHelper(time.time()).get_minute_timestamp()
+            date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(min_ts))
+            net_rate['date'] = date
+            StatusManager().hmset(name, {min_ts: json.dumps(net_rate)})
+            keys = StatusManager().hkeys(name)
+            if len(keys) > 1450:
+                keys.sort()
+                expire = keys[:len(keys) - 1440]
+                StatusManager().hmdel(name, expire)
+            gevent.sleep(60)
